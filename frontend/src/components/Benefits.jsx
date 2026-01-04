02ac2132e917a33cd0cdfd017538b5dd6c06dd26
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-01-04 03:33:20 +0200

    Create Benefits.jsx

---
+++ frontend/src/components/Benefits.jsx
@@ -0,0 +1,10 @@
+export default function Benefits() {
+  return (
+    <ul>
+      <li>✔ Beginner friendly</li>
+      <li>✔ Earn or save money</li>
+      <li>✔ Step-by-step lessons</li>
+      <li>✔ Lifetime access</li>
+    </ul>
+  );
+}
