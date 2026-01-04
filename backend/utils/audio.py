8d0e78d4aa370aa441c6713cbf18037b48f0feb2
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-01-04 03:22:10 +0200

    Update audio.py

--- backend/utils/audio.py
+++ backend/utils/audio.py
@@ -1,4 +1,10 @@
 import pyttsx3
+from pathlib import Path
+from utils.audio import text_to_audio
+
+def lesson_to_audio(text):
+    Path("media/audio").mkdir(parents=True, exist_ok=True)
+    text_to_audio(text, "media/audio/lesson1.mp3")
 
 def text_to_audio(text, filename):
     engine = pyttsx3.init()
