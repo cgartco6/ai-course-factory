491fdb95085b438f942e554d0f7b311cbd8de852
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-01-04 03:35:24 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
 import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+
+export default function App() {
+  return <Landing />;
+}
 
 export default function App() {
   return <Dashboard />;
