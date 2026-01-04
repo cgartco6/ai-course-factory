0ca7267bb7a84f4cc111a0b978cd5bb220dbd806
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-01-04 03:31:14 +0200

    Create Landing.jsx

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import Benefits from "../components/Benefits";
+import CallToAction from "../components/CallToAction";
+
+export default function Landing() {
+  return (
+    <div>
+      <Hero />
+      <Benefits />
+      <CallToAction />
+    </div>
+  );
+}
