5c857bce72b3d622f5d38a999c10abc0331d8f8e
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-01-04 12:22:33 +0200

    Create main.js

---
+++ frontend/landing/js/main.js
@@ -0,0 +1,9 @@
+function enroll() {
+    alert(
+        "Enrollment coming next.\n\n" +
+        "This button will connect to:\n" +
+        "- Gumroad\n" +
+        "- PayFast\n" +
+        "- Or backend checkout API"
+    );
+}
